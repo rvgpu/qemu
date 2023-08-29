486373014ce386608fbb7ab39d619a30177565d9
Qi Hangkong <qihangkong@outlook.com>
2023-08-29 18:14:48 +0800

    feat: add interface to acces rvgpu from r/w register and vram

--- hw/gpu/rvgpu_cif.cpp
+++ hw/gpu/rvgpu_cif.cpp
@@ -1,3 +1,5 @@
+#include <cstdio>
+
 #include "rvgpu.h"
 #include "rvgpu_cif.h"
 
@@ -9,4 +11,46 @@ void *rvgpu_create(void)
 void rvgpu_destroy(void *gpu)
 {
     delete static_cast<rvgpu *>(gpu);
+}
+
+void rvgpu_write_vram(void *gpu, uint64_t addr, uint64_t data, uint32_t size)
+{
+    rvgpu *rgpu = static_cast<rvgpu *>(gpu);
+    rgpu->write_vram(addr, data, size);
+}
+
+uint64_t rvgpu_read_vram(void *gpu, uint64_t addr, uint32_t size)
+{
+    rvgpu *rgpu = static_cast<rvgpu *>(gpu);
+    uint64_t rdata = 0;
+    rdata = (uint64_t)rgpu->read_vram(addr, size);
+
+    return rdata;
+}
+
+void rvgpu_write_register(void *gpu, uint64_t addr, uint64_t data, uint32_t size)
+{
+    rvgpu *rgpu = static_cast<rvgpu *>(gpu);
+
+    if (size != 4) {
+        printf("register need r/w with size 4\n");
+        return;
+    }
+
+    rgpu->write_register(addr, uint32_t(data));
+}
+
+uint64_t rvgpu_read_register(void *gpu, uint64_t addr, uint32_t size)
+{
+    rvgpu *rgpu = static_cast<rvgpu *>(gpu);
+    uint64_t rdata = 0;
+
+    if (size != 4) {
+        printf("register need r/w with size 4\n");
+        return rdata;
+    }
+
+    rdata = (uint64_t)rgpu->read_register(addr);
+
+    return rdata;
 }
--- hw/gpu/rvgpu_cif.h
+++ hw/gpu/rvgpu_cif.h
@@ -5,12 +5,20 @@
 #ifndef RVGPU_H__
 #define RVGPU_H__
 
+#include "stdint.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void *rvgpu_create(void);
 void rvgpu_destroy(void *gpu);
+
+
+void rvgpu_write_vram(void *gpu, uint64_t addr, uint64_t data, uint32_t size);
+uint64_t rvgpu_read_vram(void *gpu, uint64_t addr, uint32_t size);
+void rvgpu_write_register(void *gpu, uint64_t addr, uint64_t data, uint32_t size);
+uint64_t rvgpu_read_register(void *gpu, uint64_t addr, uint32_t size);
 
 #ifdef __cplusplus
 }
